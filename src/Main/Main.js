f276f5c7a678e415b6a60fe1089909a975c9e007
hwajong <hjlee1811@gmail.com>
2022-01-24 17:08:32 +0900

    수정사항 반영

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Main() {
-  return <div />;
+  return <div>Test</div>;
 }
 
 export default Main;
---
+++ src/Router.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Main from './Main/Main';
+import Mypage from './Mypage/Mypage';
+import OrderPage from './OrderPage/OrderPage';
+import ProductDetailList from './ProductDetailList/ProductDetailList';
+import ProductDetailPage from './ProductDetailPage/ProductDetailPage';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/MyPage" element={<Mypage />} />
+        <Route path="/ProductDetailList" element={<ProductDetailList />} />
+        <Route path="/ProductDetailPage" element={<ProductDetailPage />} />
+        <Route path="/OrderPage" element={<OrderPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
