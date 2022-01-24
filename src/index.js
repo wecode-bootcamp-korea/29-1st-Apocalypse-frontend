4d8aba321d2b4f59d2616eeddc05ab9271331e29
hwajong <hjlee1811@gmail.com>
2022-01-24 17:33:34 +0900

    3차 수정사항 반영

--- src/index.js
+++ src/index.js
@@ -6,9 +6,4 @@ import Router from './Router';
 import './styles/reset.scss';
 import './styles/common.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Router />, document.getElementById('root'));
