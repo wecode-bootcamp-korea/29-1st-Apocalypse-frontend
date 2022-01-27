cbd5b7941ab9abd6d0e8c88755d50e79a7d60a29
재형's <jaehyungs@jaehyeongsui-MacBookAir.local>
2022-01-27 11:10:33 +0900

    Add:My page

---
+++ src/Mypage/Warring.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Warring(props) {
+  return (
+    <div>
+      <span>아이디를 입력하세요</span>
+      <span>{props.data}</span>
+    </div>
+  );
+}
+
+export default Warring;
