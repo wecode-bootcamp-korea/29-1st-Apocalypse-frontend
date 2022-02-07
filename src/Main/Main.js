b5be3b1f07304cb82fe907957a305ab545f3714b
홍쥐 <ghdwl@hongjwiui-MacBookPro.local>
2022-02-07 13:27:59 +0900

    Modify : 메인 수정

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Footer from '../components/Footer/Footer';
 
 function Main() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Main;
