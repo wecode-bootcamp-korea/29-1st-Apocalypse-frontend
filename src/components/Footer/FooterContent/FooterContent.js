1ed2eec5f251069c258a9661992b27278301556e
홍쥐 <ghdwl@hongjwiui-MacBookPro.local>
2022-02-07 20:41:36 +0900

    Modify : 컴포넌트 수정

--- src/components/Footer/FooterContent/FooterContent.js
+++ src/components/Footer/FooterContent/FooterContent.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './FooterContent.scss';
 
 function FooterContent({ footerContent }) {
   const { title, list1, list1_link, list2, list2_link, list3, list3_link } =
     footerContent;
 
+  const dataTitle = { title };
+
   return (
     <div className="footerContent">
       <p className="footerContentTitle">{title}</p>
-      <a className="footerContentItem" href={list1_link}>
-        {list1}
-      </a>
-      <a className="footerContentItem" href={list2_link}>
-        {list2}
-      </a>
-      <a className="footerContentItem" href={list3_link}>
-        {list3}
-      </a>
+      {dataTitle === '소셜 네트워크' ? (
+        <Link to={list1_link} className="footerContentItem">
+          {list1}
+        </Link>
+      ) : (
+        <>
+          <a className="footerContentItem" href={list1_link}>
+            {list1}
+          </a>
+          <a className="footerContentItem" href={list2_link}>
+            {list2}
+          </a>
+          <a className="footerContentItem" href={list3_link}>
+            {list3}
+          </a>
+        </>
+      )}
     </div>
   );
 }
