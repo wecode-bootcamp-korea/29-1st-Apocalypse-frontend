b7005ba7d48b89fbfa2504c42fbdc9b6ba8792be
홍쥐 <ghdwl@hongjwiui-MacBookPro.local>
2022-02-07 17:59:54 +0900

    Delete : 푸터 삭제

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import FooterContent from './FooterContent/FooterContent';
-import './Footer.scss';
 
 function Footer() {
-  return (
-    <footer className="footer">
-      <div className="footerContent">
-        <FooterContent />
-        <FooterContent />
-        <FooterContent />
-      </div>
-    </footer>
-  );
+  return <div />;
 }
 
 export default Footer;
