2f75b97ceea760d8630cdedb6df13958d4eb508e
재형's <jaehyungs@jaehyeongsui-MacBookAir.local>
2022-02-10 11:50:29 +0900

    :주문기능 fetch함수 추가

--- src/OrderPage/BillSummary.js
+++ src/OrderPage/BillSummary.js
@@ -8,7 +8,7 @@ export default function BillSummary() {
 
   useEffect(() => {
     fetch('https://13.125.234.40:8080/users/cart', {
-      method: 'get',
+      method: 'GET',
       headers: {
         Authorization:
           'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMH0.JjNCEJ0NmaTH_HnbLfDkJawTeXuO6ZXwKmlKWbJoDP8',
@@ -16,29 +16,27 @@ export default function BillSummary() {
     })
       .then(res => res.json())
       .then(result => {
-        console.log(result);
+        console.log(result.carts[0]);
         setTotalPrice(result.carts[0].total_price.total_price);
       });
   }, []);
 
   return (
-    <div>
-      <div className="billSummary">
-        <h3>주문요약</h3>
-        <p>장바구니에 담겨진 수량 10</p>
+    <div className="billSummary">
+      <h3>주문요약</h3>
+      <p>장바구니에 담겨진 수량 10</p>
 
-        <div className="billPrice">
-          <span>합계</span>
-          <span>₩{totalPrice}</span>
-        </div>
-        <div className="deliveryFee">
-          <span>배송비</span>
-          <span>₩0</span>
-        </div>
-        <div className="billTotal">
-          <span>총 합계</span>
-          <span>₩{totalPrice}</span>
-        </div>
+      <div className="billPrice">
+        <span>합계</span>
+        <span>₩{totalPrice}</span>
+      </div>
+      <div className="deliveryFee">
+        <span>배송비</span>
+        <span>₩0</span>
+      </div>
+      <div className="billTotal">
+        <span>총 합계</span>
+        <span>₩{totalPrice}</span>
       </div>
     </div>
   );
--- src/OrderPage/Complete.js
+++ src/OrderPage/Complete.js
@@ -11,26 +11,24 @@ const Complete = () => {
   return (
     <div className="complete">
       <div className="compContainer">
-        <div>
-          <ol className="completeStatus">
-            <li className="statusSpace">
-              <div className="statusCheckbox2" />
-              <span className="nonState">주문서 작성</span>
-            </li>
-            <li className="statusSpace">
-              <div className="statusCheckbox2" />
-              <span className="nonState">주문 검토</span>
-            </li>
-            <li className="statusSpace">
-              <div className="statusCheckbox2" />
-              <span className="nonState">결제</span>
-            </li>
-            <li className="statusSpace">
-              <div className="statusCheckbox1" />
-              <span>주문 최종 확인</span>
-            </li>
-          </ol>
-        </div>
+        <ol className="completeStatus">
+          <li className="statusSpace">
+            <div className="statusCheckbox2" />
+            <span className="nonState">주문서 작성</span>
+          </li>
+          <li className="statusSpace">
+            <div className="statusCheckbox2" />
+            <span className="nonState">주문 검토</span>
+          </li>
+          <li className="statusSpace">
+            <div className="statusCheckbox2" />
+            <span className="nonState">결제</span>
+          </li>
+          <li className="statusSpace">
+            <div className="statusCheckbox1" />
+            <span>주문 최종 확인</span>
+          </li>
+        </ol>
         <div className="contentWrap">
           <div className="compMessage">주문이 완료되었습니다!</div>
           <button className="toMain" onClick={goToMain}>
